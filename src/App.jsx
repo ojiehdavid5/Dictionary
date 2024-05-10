37120aee4f0dee9606e2362ce2a204526ec5262f
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-10 16:08:46 +0100

    in the beginning there was a code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Content from './components/Content'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<Content/>
+      </>
+  )
+}
+
+export default App
