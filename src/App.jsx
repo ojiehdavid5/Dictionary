6c877fcbbd860740a9cf7086fc17ac3b05b000fb
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-11 16:17:54 +0100

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import Content from './components/Content'
+import Header from './components/header'
 import './App.css'
 
 function App() {
 
   return (
     <>
+<div className="className=bg-[#DBFDC8] flex flex-col items-center space-y-[6rem]">
+
+<Header/>
 <Content/>
+
+</div>
       </>
   )
 }
