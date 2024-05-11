1ead196697ccf4fc1ada0d3687a924a87f6c1337
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-05-11 16:18:03 +0100

    almost done

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
+
+
+
 
 const Content = () => {
+  const [word,setWord]=useState('');
+  console.log(word);
+  // const [result,setResult]=useState(true);
+
+
+   const HandleSubmit = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'GET',
+      url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+      params: {term: word},
+      headers: {
+        'X-RapidAPI-Key': '725fb5b942msh802001ec5ca8863p134943jsnec754daad71f',
+        'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      const data=response.data
+    } catch (error) {
+      console.error(error);
+    }
+    
+
+  }
+
+
+  
+
+  
+
+
+
+  console.log(word);
   return (
     <div>
       {/* <input
@@ -108,7 +149,7 @@ const Content = () => {
 
 
 
-<form class="px-4 w-full max-w-[330px]">
+<form class="px-4 w-full max-w-[330px]" onSubmit={HandleSubmit} >
   <label
     class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
     for="default-search"
@@ -136,13 +177,16 @@ const Content = () => {
     </div>
     <input
       required=""
-      placeholder="Search"
-      class="block w-full p-4 py-5 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 outline-none focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Search for the meaning of a word "
+      className="block w-full p-4 py-5 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 outline-none focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       id="default-search"
       type="search"
+      value={word}
+      onChange={(e)=>{setWord(e.target.value)}}
     />
     <button
       class="absolute end-2.5 bottom-1/2 translate-y-1/2 p-4 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      // onClick={setResult(false)}
     >
       <svg
         viewBox="0 0 20 20"
@@ -163,6 +207,19 @@ const Content = () => {
     </button>
   </div>
 </form>
+
+ <div className=" result ? mt-[3rem] bg-red-500 w-full h-[30rem]:hidden">
+  {data.map}
+
+
+
+
+
+
+</div> 
+
+
+
 
 
 
